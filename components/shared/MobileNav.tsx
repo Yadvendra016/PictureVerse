fa472f7e3adc7b38a2651d411ce1edb920b5a717
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2024-02-17 19:56:48 +0530

    initial commit

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <header className="header">
+      <Link href={"/"} className="flex items-center gap-2 md:py-2">
+        <Image
+          src={"/picture-verse.png"}
+          alt="logo"
+          width={180}
+          height={28}
+        ></Image>
+      </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src={"/assets/icons/menu.svg"}
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src={"/picture-verse.png"}
+                  alt="logo"
+                  width={152}
+                  height={23}
+                />
+                <ul className="header-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathname;
+                    return (
+                      <li
+                        key={link.route}
+                        className={`${
+                          isActive && "gradient-text"
+                        } p-18 flex whitespace-nowrap text-dark-700`}
+                      >
+                        <Link
+                          className="sidebar-link cursor-pointer"
+                          href={link.route}
+                        >
+                          <Image
+                            src={link.icon}
+                            alt="icon"
+                            width={24}
+                            height={24}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+
+        {/* if signed out */}
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href={"/sign-in"}></Link>
+          </Button>
+        </SignedOut>
+      </nav>
+    </header>
+  );
+};
+
+export default MobileNav;
---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href={"/"} className="sidebar-logo">
+          <Image
+            src={"/picture-verse.png"}
+            alt="logo"
+            width={180}
+            height={180}
+          />
+        </Link>
+        <nav className="sidebar-nav">
+          {/* if we signed in */}
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.slice(0, 6).map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="icon"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <ul className="sidebar-nav_elements border-t border-black">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="icon"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl="/" showName />
+              </li>
+            </ul>
+          </SignedIn>
+
+          {/* if we signed out */}
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href={"/sign-in"}></Link>
+            </Button>
+          </SignedOut>
+        </nav>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
