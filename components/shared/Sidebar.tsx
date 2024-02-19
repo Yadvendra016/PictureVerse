29197c54e55f1b263d356697bd3804b27a965128
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2024-02-19 21:42:27 +0530

    Make some fixes

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -84,7 +84,7 @@ const Sidebar = () => {
           {/* if we signed out */}
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href={"/sign-in"}></Link>
+              <Link href={"/sign-in"}>LOG IN</Link>
             </Button>
           </SignedOut>
         </nav>
